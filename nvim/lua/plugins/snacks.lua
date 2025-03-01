50dae5548ad2b8330ed9f115f41aef50dd8c7a86
albinlju <albin.ljung@telia.com>
2025-03-01 21:42:56 +0000

    changing dashboard

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,6 +3,21 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+        {
+          section = "terminal",
+          cmd = "ascii-image-converter ~/dotfiles/computer.png -c -C",
+          random = 5,
+          pane = 2,
+          indent = 4,
+          height = 30,
+        },
+      },
+    },
   },
 }
