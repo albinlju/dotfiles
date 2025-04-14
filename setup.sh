ec05acb9276429ebf3408f441167487e99534d32
albinlju <albin.ljung@telia.com>
2025-04-14 22:40:04 +0000

    readded bashrc

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 ln -sf "$PWD/starship.toml" "$XDG_CONFIG_HOME"/starship.toml
 ln -sf "$PWD/.inputrc" "$HOME"/.inputrc
-# ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
+ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
 ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
 
