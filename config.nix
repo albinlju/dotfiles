80889b329f276e833cf457b7fdaab28f335db2fb
Albin <41971986+albinlju@users.noreply.github.com>
2025-04-30 22:38:46 +0200

    Update config.nix
    try setting neovim version

--- config.nix
+++ config.nix
@@ -4,7 +4,7 @@
       name = "albinlju-packages";
       paths = [
         zsh-completions
-        neovim
+        (neovim.overrideAttrs (oldAttrs: { version = "0.10.4"; src = oldAttrs.src; }))
         dwt1-shell-color-scripts
         pure-prompt
         starship
