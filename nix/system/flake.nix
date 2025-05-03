85dc191a094681bdacbe5974abc1f690b93223b3
Albin <41971986+albinlju@users.noreply.github.com>
2025-05-03 19:25:22 +0200

    Update flake.nix

--- nix/system/flake.nix
+++ nix/system/flake.nix
@@ -1,24 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.mkFlake {
-      inherit self nixpkgs;
-      systems = [ "aarch64-linux" ];
-
-      packages.aarch64-linux.default = let pkgs = nixpkgs.legacyPackages.aarch64-linux; in [
-        pkgs.neovim
-        pkgs.starship
-        pkgs.tmux
-        pkgs.fd
-        pkgs.ripgrep
-        pkgs.fzf
-        pkgs.lazygit
-        pkgs."dwt1-shell-color-scripts"
-        pkgs.asciiquarium
-      ];
-   };
+  outputs = { nixpkgs, ... }:
+  let
+    pkgs = nixpkgs.legacyPackages.aarch64-linux;
+  in
+  {
+    packages.aarch64-linux.default = [
+      pkgs.neovim
+      pkgs.starship
+      pkgs.tmux
+      pkgs.fd
+      pkgs.ripgrep
+      pkgs.fzf
+      pkgs.lazygit
+      pkgs."dwt1-shell-color-scripts"
+      pkgs.asciiquarium
+    ];
+  };
 }
