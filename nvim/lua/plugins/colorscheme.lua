5066877a42fd360100ba92d02cfe61fb11c0ea69
albinlju <albin.ljung@telia.com>
2025-05-08 19:45:46 +0000

    house cleaning

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,30 +1,7 @@
 return {
-
   {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = { style = "moon" },
   },
-
-  -- {
-  --   "rebelot/kanagawa.nvim",
-  --   lazy = true,
-  --   opts = {
-  --    colors = {
-  --       theme = {
-  --         all = {
-  --           ui = {
-  --             bg_gutter = "none",
-  --           },
-  --        },
-  --     },
-  --    },
-  --  },
-  -- },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "kanagawa-dragon",
-  --   },
-  ---- },
 }
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,6 +3,7 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
+    explorer = { enabled = false },
     dashboard = {
       enabled = true,
       preset = {
