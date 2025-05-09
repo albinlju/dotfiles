22cb84b6b43a933e47952d447b3d1cb52fb1ebff
= <=>
2025-05-09 20:18:11 +0000

    changes

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,15 +1,3 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, {
-        function()
-          return "😄"
-        end,
-      })
-    end,
-  },
 }
