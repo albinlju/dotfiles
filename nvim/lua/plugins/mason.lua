18ea3cd7d33d406f40d436414b1b6cb384c238b3
= <=>
2025-05-27 20:34:42 +0000

    added registries for mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -5,6 +5,10 @@ return {
     "williamboman/mason-lspconfig.nvim",
     branch = "v1.x",
   },
+  registries = {
+    "github:mason-org/mason-registry",
+    "github:Crashdummyy/mason-registry",
+  },
   opts = {
     ensure_installed = {
       "stylua",
