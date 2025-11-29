cea94cbfd6433cc8aca4af7492ac3ee8f5783cf1
Albin Ljung <albinljung@Albins-MacBook-Pro.local>
2025-11-29 14:04:16 +0100

    latest neovim and go

--- nix/profiles/base-colima-devpod/flake.nix
+++ nix/profiles/base-colima-devpod/flake.nix
@@ -2,17 +2,21 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        nightlyPkgs = neovim-nightly.packages.${system};
       in
       {
         defaultPackage = pkgs.buildEnv {
           name = "system-packages";
-          paths = [ 
+          paths = [
+            nightlyPkgs.default 
+            pkgs.go
             pkgs.colima
             pkgs.docker
             pkgs.devpod
