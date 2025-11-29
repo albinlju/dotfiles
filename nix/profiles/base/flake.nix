2b6caea6614651c2bcdaf5aa3e5fc28dabda1c9e
Albin Ljung <albinljung@Albins-MacBook-Pro.local>
2025-11-29 14:00:21 +0100

    added go

--- nix/profiles/base/flake.nix
+++ nix/profiles/base/flake.nix
@@ -2,18 +2,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        nightlyPkgs = neovim-nightly.packages.${system};
       in
       {
         defaultPackage = pkgs.buildEnv {
           name = "system-packages";
           paths = [ 
-            pkgs.neovim 
+            nightlyPkgs.default
             pkgs.starship
             pkgs.tmux 
             pkgs.git
