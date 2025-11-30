9d93243d4e13d1e4b2867c33511f9986845bc2d6
Albin Ljung <albinljung@Albins-MacBook-Pro.local>
2025-11-30 23:59:49 +0100

    cleaning

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,5 +1,3 @@
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -13,13 +11,7 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
--- NOTE: Here is where you install your plugins.
---
 require("lazy").setup({
   spec = { { import = "plugins" } },
   checker = { enabled = true },
 })
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
