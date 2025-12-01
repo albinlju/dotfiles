357bfd8fc7c3d8957f56e3f89da324f773856480
Albin Ljung <albinljung@Albins-MacBook-Pro.local>
2025-12-01 01:32:25 +0100

    small tweaks

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,33 @@
 return { -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  main = "nvim-treesitter.configs", -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+  main = "nvim-treesitter.configs",
   opts = {
-    ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc" },
-    -- Autoinstall languages that are not installed
+    ensure_installed = {
+      "bash",
+      "c",
+      "tsx",
+      "javascript",
+      "diff",
+      "json",
+      "razor",
+      "scss",
+      "yaml",
+      "c_sharp",
+      "vue",
+      "html",
+      "lua",
+      "go",
+      "gomod",
+      "gowork",
+      "gosum",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "query",
+      "vim",
+      "vimdoc",
+    },
     auto_install = true,
     highlight = {
       enable = true,
@@ -16,10 +38,4 @@ return { -- Highlight, edit, and navigate code
     },
     indent = { enable = true, disable = { "ruby" } },
   },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
