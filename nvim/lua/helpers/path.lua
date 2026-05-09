6de90440cb7f5c403ea5ddb8cf8c4c4e516dd89f
albinljung <albin.ljung@consid.se>
2026-05-09 02:06:46 +0200

    make function dynamic

--- nvim/lua/helpers/path.lua
+++ nvim/lua/helpers/path.lua
@@ -8,8 +8,14 @@ function M.get_nix_profile_binary_path(name)
   handle:close()
   local data = vim.json.decode(result)
 
-  local store_path = data.elements["nix/profiles/base"].storePaths[1]
-  --assert(store_path == nil, "Failed to get base path code:" .. vim.inspect(store_path))
+  local store_path
+  for _, element in pairs(data.elements) do
+    if element.active then
+      store_path = element.storePaths[1]
+      break
+    end
+  end
+  assert(store_path, "Failed to find active nix profile element")
 
   return store_path .. "/bin/" .. name
 end
