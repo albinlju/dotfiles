c65c59e5a2abe85684acef0707cef9c7b0674237
albinljung <albin.ljung@consid.se>
2026-05-09 02:08:27 +0200

    use right rust plugin

---
+++ nvim/lua/plugins/rust.lua
@@ -0,0 +1,19 @@
+return {
+  "mrcjkb/rustaceanvim",
+  version = "^6",
+  lazy = false,
+  init = function()
+    vim.g.rustaceanvim = {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "<C-space>", function()
+            vim.cmd.RustLsp({ "hover", "actions" })
+          end, { buffer = bufnr })
+          vim.keymap.set("n", "<Leader>a", function()
+            vim.cmd.RustLsp("codeAction")
+          end, { buffer = bufnr })
+        end,
+      },
+    }
+  end,
+}
